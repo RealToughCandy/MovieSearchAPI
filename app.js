8ed720c8f9bf84c7deb67198266683b0643615b8
RealToughCandy <barncatdigital@gmail.com>
2017-11-13 02:14:06 -0600

    added flexbox

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var request = require('request');
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/'));
+
 
 // app has two routes: search & results.
 
